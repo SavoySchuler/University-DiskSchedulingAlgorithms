8ab54787039d9c4bc9913080293a28a0c823e1bd
sds3 <savoy.schuler@ravenind.com>
2017-04-27 15:58:27 -0600

    Updated commenting on main file.

--- disk_sched.cpp
+++ disk_sched.cpp
@@ -1,4 +1,7 @@
+/// Include program .h for disk size and num request defines.
 #include "disk.h"
+
+/// Include disk scheduling algorithms to be simulated.
 #include "c-look.cpp"
 #include "c-scan.cpp"
 #include "fcfs.cpp"
@@ -6,18 +9,22 @@
 #include "scan.cpp"
 #include "sstf.cpp"
 
+/// Include library for cout output.
 #include <iostream>
+
+/// Include library for vectors.
 #include <vector>
 
 /// Include libraries for use of rand. 
 #include <stdlib.h>
 #include <time.h>
 
+/// Using standard namespace. 
 using namespace std;
 
 vector<int> populate_request_queue()
 {
-/*    /// Initialize iterator variable.
+    /// Initialize iterator variable.
     int i = 0;
 
     /// Initialize temporary queue for holding random cylinder requests.
@@ -32,29 +39,27 @@ vector<int> populate_request_queue()
         /** Simulate random cylinder request by generating random number and 
         modding by max cylinder address. Push value to back of request queue.
         Request queue should be of length NUM_REQUESTS by end of loop.
-----/
+        */
         request_queue.push_back(rand()%(DISK_SIZE-1));
     }
 
     /// Return the temporary request queue to main.
-    return request_queue;*/
-
-    vector<int> request_queue;
-
-    int myints[] = {98, 183, 37, 122, 14, 124, 65, 67};
-    request_queue.assign (myints,myints+8);
-
     return request_queue;
 }
 
 int main( int argc, char *argv[])
 {
+    /// Check for proper number of command line parameters entered.
     if (argc != 2) 
     {
+        /// If not, print usage to user.
         cerr << "\nUsage: " << argv[0] << " <int>\n\n";
+        
+        /// Exit with error.
         exit(1);
     }
 
+    /// Convert user-entered starting head position to int.
     int head = atoi(argv[1]);
 
     /** Create multiple copies of a randomly generated request queue so that 
@@ -87,14 +92,17 @@ int main( int argc, char *argv[])
         exit(1);
     }
 
-
+    /// Print to user header info and simulation parameters. 
     cout << "\nDISK SCHEDULING ALGORITHMS:\n\n" <<
         "Initial position of disk head: " << head << "\n"
         "Num Cylinders on disk: " << DISK_SIZE << "\n" <<
         "Num Random Cylinder Requests: " << NUM_REQUESTS << "\n\n" <<
         "HEAD MOVEMENT REQUIRED PER ALGORITHM:\n\n";
 
-
+    /** 
+    Call each algorithm simulation on the same random requests and output
+    total head movement for each.
+    */ 
     cout << "FCFS: " << fcfs(request_queue_fcfs, head) << "\n\n";
 
     cout << "SSTF: " << sstf(request_queue_sstf, head) << "\n\n";
@@ -107,6 +115,8 @@ int main( int argc, char *argv[])
 
     cout << "C-LOOK: " << c_look(request_queue_c_look, head) << "\n\n";
 
+    /// Exit normally.
+    return 0;
 }
 
 /*
